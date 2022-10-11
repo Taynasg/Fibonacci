cd97ed07c799fb967b0e3b9cd5ff07a8799114eb
Tayná <taynasg@ciandt.com>
2022-10-11 14:19:26 -0300

    Primeiro commit

---
+++ src/Fibonacci.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+//75) Crie um programa que preencha automaticamente 
+//(usando lógica, não apenas atribuindo diretamente) 
+//um vetor numérico com 15 posições com os primeiros
+//elementos da sequência de Fibonacci:
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("Informe um número para iniciar a sequência de"
+			+ "Fibonacci: ");
+	
+	int numero = scanner.nextInt(); 
+	
+	int vetor[] = new int [15];
+	vetor[0] = 1;
+	
+	
+	
+	
+	}
+
+}
